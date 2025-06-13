157c97f064f605713132823f1c0f80e89b91bc9a
gabs <1gabrielbprado@gmail.com>
2025-06-13 17:06:15 -0400

    first commit

---
+++ Resumo_analise_sentimentos.py
@@ -0,0 +1,22 @@
+### Análise de texto e respostas e perguntas
+# Através do idioma e frases chave utilizados, a análise pode ser feita utilizando-se do sentimento detectado (como positivo, por exemplo, em espectro), e das entidades descritas.
+
+# Já na parte de respostas a perguntas, deve-se definir uma base de conhecimento de pares de perguntas e respostas, quando:
+# Insere-se perguntas e respostas
+# De um documento de perguntas frequentes já existente
+# Ou utilizando-se de um bate papo integrado
+
+### Serviço de bot do azureAI
+# São necessárias muitas informações para o funcionamento correto do bot, logo, a base de dados deve ser o mais vasta possível;
+# A resposta é criada com base nas palavras chaves utilizadas na pergunta;
+# É importante testar e retestar o bot várias vezes;
+
+# É necessária uma plataforma baseada em nuvem para desenvolvimento e gerenciamento de bots, com integração com AI Language e outros serviços e que se disponibilize de conectividade através de vários outros canais para quando necessários;
+
+### Compreensão da linguagem coloquial
+# Linguagem mais do dia a dia sem termos formais;
+# Formado por 03 componentes principais = declaração, entidades, intenção;
+# Reconhecimento e síntese de fala: use os recursos de fala para texto do serviço Fala para transcrever fala audível em texto, disponível em mais de 60 idiomas;
+# Dessa conversão de texto em fala, gera-se uma fala audível a partir de texto;
+
+### Apresentação do Estudio de fala (Language studio)
